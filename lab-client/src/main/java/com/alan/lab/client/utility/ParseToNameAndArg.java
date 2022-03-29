3d0e5a84f57ac56dfd276dc51f1b58ec3c7d0662
lm-cyber <tyujn24@mail.ru>
2022-03-29 14:17:39 +0300

    norm

--- lab-client/src/main/java/com/alan/lab/client/utility/ParseToNameAndArg.java
+++ lab-client/src/main/java/com/alan/lab/client/utility/ParseToNameAndArg.java
@@ -3,18 +3,11 @@ package com.alan.lab.client.utility;
 public class ParseToNameAndArg {
     private String name;
     private String arg;
-    public ParseToNameAndArg(String line)
-    {
+
+    public ParseToNameAndArg(String line) {
         String[] lineSplit = line.split(" ", 2);
         this.name = lineSplit[0];
-        this.arg = lineSplit.length==2?lineSplit[1] : "";
-    }
-
-    public String[] getAll() {
-        String[] all = new String[2];
-        all[0] = name;
-        all[1] = arg;
-        return all;
+        this.arg = lineSplit.length == 2 ? lineSplit[1] : "";
     }
 
     public String getArg() {
